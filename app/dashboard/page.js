f29764e73c11ac6c65966063a5e42c228882cd5e
Josef Moravec <mory@Josefs-MacBook-Pro-2.local>
2025-04-01 13:28:34 +0700

    HOTFIX: Load Builder manually from CDN

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { BuilderComponent, builder } from '@builder.io/react'
-
 builder.init('c68c3c02570a4745a92bbb7557b1e04c')
 
 export default function DashboardPage() {
--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 'use client'
-import { BuilderComponent, builder } from '@builder.io/react'
-
-builder.init('c68c3c02570a4745a92bbb7557b1e04c') // tvůj veřejný klíč
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-    <div>
-      <BuilderComponent model="page" content={{ url: '/' }} />
-    </div>
-  )
+  const [html, setHtml] = useState('')
+
+  useEffect(() => {
+    fetch('https://cdn.builder.io/api/v1/html/gpt-victory?url=/&apiKey=c68c3c02570a4745a92bbb7557b1e04c')
+      .then(res => res.text())
+      .then(setHtml)
+  }, [])
+
+  if (!html) {
+    return <p style={{ color: 'white', padding: 20 }}>Loading landing page…</p>
+  }
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
