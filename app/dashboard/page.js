b69f4f39f325ef1c0e210e99517dcdefb3ec2f6d
Josef Moravec <mory@Josefs-MacBook-Pro-2.local>
2025-04-01 14:54:53 +0700

    Load Builder landing + dashboard by entry ID

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,10 @@
 'use client'
 import { BuilderComponent, builder } from '@builder.io/react'
+
 builder.init('c68c3c02570a4745a92bbb7557b1e04c')
 
-export default function DashboardPage() {
+export default function Dashboard() {
   return (
-    <div>
-      <BuilderComponent model="page" content={{ url: '/dashboard' }} />
-    </div>
+    <BuilderComponent model="page" content={{ entry: '927e74aae4d74ab6a506f8f04d8847e2' }} />
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 'use client'
 import { BuilderComponent, builder } from '@builder.io/react'
+
 builder.init('c68c3c02570a4745a92bbb7557b1e04c')
 
 export default function Home() {
   return (
-    <div>
-      <BuilderComponent model="page" content={{ url: '/' }} />
-    </div>
+    <BuilderComponent model="page" content={{ entry: '79f338dda63f4da79876830729b23892' }} />
   )
 }
