0aee30f1a011f5bea13f247b7b456c58b1c3c3cb
Josef Moravec <mory@Josefs-MacBook-Pro-2.local>
2025-04-01 14:43:20 +0700

    Fix: connect to Builder 'page' model live homepage

--- app/page.js
+++ app/page.js
@@ -1,22 +1,10 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { BuilderComponent, builder } from '@builder.io/react'
+
+builder.init('c68c3c02570a4745a92bbb7557b1e04c')
 
 export default function Home() {
-  const [html, setHtml] = useState('')
-
-  useEffect(() => {
-    fetch('https://cdn.builder.io/api/v1/html/page?url=/&apiKey=c68c3c02570a4745a92bbb7557b1e04c')
-      .then(res => res.text())
-      .then(setHtml)
-  }, [])
-
   return (
-    <main>
-      {html ? (
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      ) : (
-        <p style={{ color: 'white', padding: 32 }}>Loading content from Builder...</p>
-      )}
-    </main>
+    <BuilderComponent model="page" content={{ url: '/' }} />
   )
 }
