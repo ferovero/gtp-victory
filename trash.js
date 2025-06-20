55b37f37c47a3f092032825d82d567e54ca4b19e
shubham sharma <wifibri2@gmail.com>
2025-06-20 11:20:03 +0530

    lazy commit

---
+++ trash.js
@@ -0,0 +1,133 @@
+{/* New chat Refrences */ }
+{/* <div
+                style={{
+                  marginBottom: "0.75rem",
+                }}
+              >
+                {latestCreatedConversation?.id && (
+                  <ConversationItem
+                    key={latestCreatedConversation.id}
+                    conversation={latestCreatedConversation}
+                    editable={false}
+                  />
+                )}
+              </div> */}
+
+// Dashboard Layout
+//   const latestCreatedConversation = useMemo(() => {
+//     const conversations = conversationsData?.conversations;
+//     return !conversationsLoading && conversations?.length > 0
+//       ? conversations.filter((i) => i?._count?.messages == 0)[0]
+//       : null;
+//   }, [conversationsData]);
+
+//   const chatSlugConversation = useMemo(() => {
+//     const conversations = conversationsData?.conversations;
+//     return !conversationsLoading && conversations?.length > 0
+//       ? conversations.filter((i) => i.id == slug)[0]
+//       : null;
+//   }, [conversationsData, slug]);
+
+
+// //   Window scroll to top
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   });
+
+// Dashboard layout 28 no line approx
+
+//   check is the latest post have any message if not means it is blank space then no create any conversation space
+//   useEffect(() => {
+//     if (latestCreatedConversation?._count?.messages > 0) {
+//       // creating new conversation space
+//       createConversation(null);
+//     } else {
+//       setSelectedConversation(latestCreatedConversation);
+//     }
+//   }, [latestCreatedConversation]);
+
+
+
+
+
+
+
+
+
+
+
+// ConversationItem
+//   if (!editable) {
+//     return (
+//       <button
+//         key={conversation.id}
+//         className={css.css_9rto4r_builder_block}
+//         style={{ background: "transparent" }}
+//         onClick={handleConversationClick}
+//       >
+//         <span className="builder-block builder-9a1c16654f844cf7b9724e6aa4c438c7 builder-has-component css-vky7x4">
+//           <span className={css.conversation_name}>{conversation.title}</span>
+//         </span>
+//       </button>
+//     );
+//   }
+
+
+
+
+
+
+
+
+
+{/* Loading Button */ }
+{/* {conversationsLoading && (
+          <div
+            className={css.css_1k2cfop_builder_block}
+            builder-id="builder-07af320d54a04e37afcbca91e939cc3f"
+          >
+            <Loader className={css.spin_loader} />
+            <span
+              className="builder-block builder-ef542ba300ad4192b22cfc1150e2727f builder-has-component css-y2p0nr"
+              builder-id="builder-ef542ba300ad4192b22cfc1150e2727f"
+            >
+              <span className="builder-text css-1qggkls"> Loading...</span>
+            </span>
+          </div>
+        )}  */}
+
+
+// Dashboard layout
+
+
+// ?? Initial load of page = 1 only
+//   useEffect(() => {
+//     if (sidebarHeight > 0 && initialLoad) {
+//       setInitialLoad(false);
+//       //   debugger;
+//       console.log("Setting Page :: Sidebar Height ");
+//       setPage((prev) => prev + 1);
+//     }
+//   }, [sidebarHeight, initialLoad]);
+
+
+// ?? to ensure when user give any slug then increase page to get tthat slug in conversations
+//   useEffect(() => {
+//     if (chatSlugConversation?.page) {
+//       for (let page = chatSlugConversation.page; page >= 1; page--) {
+// debugger;
+//         console.log("Page updating in Chat Slug Conversation ");
+//         setPage(page);
+//       }
+//     }
+//   }, [chatSlugConversation]);
+
+
+
+    // console.log(conversationsData?.conversations);
+    // console.log(
+    //   "When Page",
+    //   page,
+    //   "then conversations is:",
+    //   conversationsData?.conversations
+    // );
